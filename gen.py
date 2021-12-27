b714700d827e038d8afd9a7dbb6fa87ab908f91a
MichiTheGenius <litzinger04@gmail.com>
2021-12-27 14:32:11 +0100

    indented everything with vscode

--- gen.py
+++ gen.py
@@ -2,21 +2,21 @@ import qrcode
 import argparse
 
 
-
 class Generator():
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='QRCodeGen program')
-        self.parser.add_argument('name', type=str, help='name of output QR code')
-        self.parser.add_argument('content', type=str, help='content of QR code')
+        self.parser.add_argument(
+            'name', type=str, help='name of output QR code')
+        self.parser.add_argument(
+            'content', type=str, help='content of QR code')
         self.args = self.parser.parse_args()
-    
-    def makeQR(self, name, msg): 
+
+    def makeQR(self, name, msg):
         img = qrcode.make(msg)
         img.save(name)
 
     def generate(self):
         self.makeQR(self.args.name, self.args.content)
-
 
 generator = Generator()
 
